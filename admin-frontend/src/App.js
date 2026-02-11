630feec4082466848d131b63ba55ec1a199ca93e
Wei Wang <2696674011@qq.com>
2026-02-11 16:45:55 +0800

    完整功能框架2

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,7 +1,20 @@
 import RouterConfig from "./router";
+import { ConfigProvider } from "antd";
 
 function App() {
-  return <RouterConfig />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#1677ff",
+          colorBgBase: "#f0f6ff",
+          borderRadius: 8
+        }
+      }}
+    >
+      <RouterConfig />
+    </ConfigProvider>
+  );
 }
 
 export default App;
